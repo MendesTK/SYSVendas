c01c87b2ba32504f98c6f6ea35d0da347022cf35
Murilo Begnini <murilobegnini@gmail.com>
2019-05-06 08:08:10 -0300

    Ajustes

--- SYSVendas.MVC/App_Start/BundleConfig.cs
+++ SYSVendas.MVC/App_Start/BundleConfig.cs
@@ -12,12 +12,9 @@ namespace SYSVendas.MVC
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        //"~/Scripts/jquery.validate*",
-                        "~/Scripts/jquery.validate-vsdoc.js",
-                        "~/Scripts/jquery.validate.js",
-                        "~/Scripts/jquery.validate.unobstrusive.js",
-                        "~/Scripts/globalize.js",
-                        "~/Scripts/jquery.validate.globalize.js"));
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/methods_pt.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -27,7 +24,6 @@ namespace SYSVendas.MVC
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                         "~/Scripts/moment.js",
                         "~/Scripts/bootstrap-datetimepicker.js",
-                        "~/Scripts/bootstrap-datetimepicker.min.js",
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
